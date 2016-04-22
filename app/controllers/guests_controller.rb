759b1fa9b9b7b891a49b2066b9f4364cc0b34d16
Dev Eggers <eggers.brendan@gmail.com>
2016-04-22 11:25:59 -0400

    Creates Guests/Orders and Deletes Orders from a guests recipt

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -3,12 +3,22 @@ class GuestsController < ApplicationController
   def show
     @guest = Guest.find params[:id]
     @orders = Order.where guest_id: @guest.id
-  end
 
+    @total = 0
+    @orders.each do |order|
+      @total += order.item.price
+    end
+  end
 
   def create
     guest = Guest.create guest_params
     redirect_to table_path(guest.table_id)
+  end
+
+
+  def destroy
+    Guest.destroy @guest.id
+    redirect_to table_path(@guest.table_id)
   end
 
   private
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,8 +2,16 @@ class OrdersController < ApplicationController
 
   def create
     order = Order.create order_params
-    redirect_to guest_items_path(params[:id])
+    redirect_to guest_items_path(order.guest_id)
   end
+
+  def destroy
+    order = Order.find params[:id]
+    guest = Guest.find order.guest_id
+    Order.destroy order
+    redirect_to guest_path(guest.id)
+  end
+
 
   private
   def order_params
